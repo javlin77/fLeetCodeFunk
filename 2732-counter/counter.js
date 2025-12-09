af59960c70ea2b7786d63eef39fa8fc67bbf20a2
Upal Pahari <upalthisside@gmail.com>
2025-12-09 23:13:52 +0530

    Time: 51 ms (23.00%) | Memory: 53.4 MB (87.89%) - LeetSync

--- 2732-counter/counter.js
+++ 2732-counter/counter.js
@@ -2,12 +2,10 @@
  * @param {number} n
  * @return {Function} counter
  */
-var createCounter = function (n) {
-    let currentCount = n - 1;
-    return function () {
-        currentCount += 1;
-        return currentCount;
-    };
+var createCounter = function(n) {
+  return function() {
+    return n++;      
+  };
 };
 
 /** 
