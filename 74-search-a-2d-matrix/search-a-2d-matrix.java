2314f02e5a0b6da48cc065d6339d901e15c81342
Upal Pahari <upalthisside@gmail.com>
2025-12-11 22:43:35 +0530

    Time: 0 ms (100.00%) | Memory: 43.7 MB (47.37%) - LeetSync

---
+++ 74-search-a-2d-matrix/search-a-2d-matrix.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0)
+            return false;
+
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        int low = 0, high = rows * cols - 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            int r = mid / cols; 
+            int c = mid % cols;   
+            int num = matrix[r][c];
+            if (num == target) {
+                return true;
+            } else if (num < target) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return false;
+    }
+}
