1cedabaf4593ff1a39cbfb2884e965ed9fdd8f72
Upal Pahari <upalthisside@gmail.com>
2026-03-29 11:01:19 +0530

    Time: 1 ms (100.00%) | Memory: 44.5 MB (25.51%) - LeetSync

--- 2999-check-if-strings-can-be-made-equal-with-operations-i/check-if-strings-can-be-made-equal-with-operations-i.java
+++ 2999-check-if-strings-can-be-made-equal-with-operations-i/check-if-strings-can-be-made-equal-with-operations-i.java
@@ -1,7 +1,5 @@
 class Solution {
     public boolean canBeEqual(String s1, String s2) {
-        if (s1.length() != s2.length()) return false;
-
         int[] count = new int[26];
         for (int i = 0; i < s1.length(); i++) {
             count[s1.charAt(i) - 'a']++;
