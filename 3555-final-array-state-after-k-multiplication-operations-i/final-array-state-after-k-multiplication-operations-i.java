895619ec755143563d27d986066b2c241b052ddb
Upal Pahari <upalthisside@gmail.com>
2026-04-10 10:09:52 +0530

    Time: 3 ms (29.23%) | Memory: 46.7 MB (66.17%) - LeetSync

--- 3555-final-array-state-after-k-multiplication-operations-i/final-array-state-after-k-multiplication-operations-i.java
+++ 3555-final-array-state-after-k-multiplication-operations-i/final-array-state-after-k-multiplication-operations-i.java
@@ -1,15 +1,26 @@
-class Solution {
+ class Solution {
+
     public int[] getFinalState(int[] nums, int k, int multiplier) {
-        int n = nums.length;
-        for (int j = 0; j < k; j++) {
-            int minIndex = 0;
-            for (int i = 1; i < n; i++) {
-                if (nums[i] < nums[minIndex]) {
-                    minIndex = i;
-                }
+        PriorityQueue<int[]> heap = new PriorityQueue<>((a, b) -> {
+            int valueComparison = Integer.compare(a[0], b[0]);
+            if (valueComparison == 0) {
+                return Integer.compare(a[1], b[1]);
             }
-            nums[minIndex] *= multiplier;
+            return valueComparison;
+        });
+
+        for (int i = 0; i < nums.length; i++) {
+            heap.offer(new int[] { nums[i], i });
         }
+
+        while (k-- > 0) {
+            int[] smallest = heap.poll();
+            int index = smallest[1];
+
+            nums[index] *= multiplier;
+            heap.offer(new int[] { nums[index], index });
+        }
+
         return nums;
     }
 }
