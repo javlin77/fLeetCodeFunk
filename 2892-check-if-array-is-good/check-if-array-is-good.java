5e1744d26a9657a76118a06c3a3a1079470a5d1a
Upal Pahari <upalthisside@gmail.com>
2026-05-14 13:18:20 +0530

    Time: 0 ms (100.00%) | Memory: 44.6 MB (78.29%) - LeetSync

--- 2892-check-if-array-is-good/check-if-array-is-good.java
+++ 2892-check-if-array-is-good/check-if-array-is-good.java
@@ -1,12 +1,19 @@
 class Solution {
     public boolean isGood(int[] nums){
-        Arrays.sort(nums);
-        int n = nums.length - 1;
-        for (int i = 0; i < n; i++) {
-            if (nums[i] != i + 1) {
+        int n = nums.length;
+        int[] count = new int[n];
+        for (int i : nums) {
+            if (i >= n) {
                 return false;
             }
+            if (i < n - 1 && count[i] > 0) {
+                return false;
+            }
+            if (i == n - 1 && count[i] > 1) {
+                return false;
+            }
+            count[i]++;
         }
-        return nums[n] == n;
+        return true;
     }
 }
