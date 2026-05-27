8feddb9fb582e8b7cb6ac4bac921f0ef0f12e0a7
Upal Pahari <upalthisside@gmail.com>
2026-05-27 22:14:50 +0530

    Time: 563 ms (56.84%) | Memory: 0B (100.00%) - LeetSync

--- 1182-game-play-analysis-iv/game-play-analysis-iv.sql
+++ 1182-game-play-analysis-iv/game-play-analysis-iv.sql
@@ -2,13 +2,16 @@
 
 SELECT
     ROUND(
-        COUNT(DISTINCT player_id) /
+        COUNT(DISTINCT a1.player_id) /
         (SELECT COUNT(DISTINCT player_id) FROM Activity),
     2) AS fraction
-FROM Activity
-WHERE (player_id, DATE_SUB(event_date, INTERVAL 1 DAY))
-IN (
-    SELECT player_id, MIN(event_date)
+FROM Activity a1
+JOIN (
+    SELECT
+        player_id,
+        MIN(event_date) AS first_login
     FROM Activity
     GROUP BY player_id
-);
+) a2
+ON a1.player_id = a2.player_id
+AND DATE_SUB(a1.event_date, INTERVAL 1 DAY) = a2.first_login;
