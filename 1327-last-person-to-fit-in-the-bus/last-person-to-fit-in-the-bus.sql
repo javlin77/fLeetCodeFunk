9560adb9dbdab955ba87c9a304a53b886772127e
Upal Pahari <upalthisside@gmail.com>
2026-05-28 20:23:03 +0530

    Time: 2083 ms (5.00%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1327-last-person-to-fit-in-the-bus/last-person-to-fit-in-the-bus.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+SELECT q1.person_name
+FROM Queue q1
+JOIN Queue q2
+ON q2.turn <= q1.turn
+GROUP BY q1.turn, q1.person_name
+HAVING SUM(q2.weight) <= 1000
+ORDER BY q1.turn DESC
+LIMIT 1;
