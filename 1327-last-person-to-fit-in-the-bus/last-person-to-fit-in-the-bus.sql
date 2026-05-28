4b515ca515ce6e16a9ca3c763833f6b6f83a354e
Upal Pahari <upalthisside@gmail.com>
2026-05-28 20:30:09 +0530

    Time: 2465 ms (5.00%) | Memory: 0B (100.00%) - LeetSync

--- 1327-last-person-to-fit-in-the-bus/last-person-to-fit-in-the-bus.sql
+++ 1327-last-person-to-fit-in-the-bus/last-person-to-fit-in-the-bus.sql
@@ -3,7 +3,7 @@ SELECT q1.person_name
 FROM Queue q1
 JOIN Queue q2
 ON q2.turn <= q1.turn
-GROUP BY q1.turn, q1.person_name
+GROUP BY q1.turn, q1.person_name, q1.person_id
 HAVING SUM(q2.weight) <= 1000
 ORDER BY q1.turn DESC
 LIMIT 1;
