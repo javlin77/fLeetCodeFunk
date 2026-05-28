62ba57450eea778b5a8c1212c153c85cbe41e61c
Upal Pahari <upalthisside@gmail.com>
2026-05-28 11:18:32 +0530

    Time: 553 ms (27.18%) | Memory: 0B (100.00%) - LeetSync

--- 1523-capital-gainloss/capital-gainloss.sql
+++ 1523-capital-gainloss/capital-gainloss.sql
@@ -1,9 +1,7 @@
 # Write your MySQL query statement below
-SELECT stock_name, SUM(
-    CASE
-        WHEN operation = 'Sell' THEN price
-        ELSE -price
-    END
-) AS capital_gain_loss
+SELECT stock_name,
+       SUM(
+           IF(operation = 'Sell', price, -price)
+       ) AS capital_gain_loss
 FROM Stocks
-GROUP BY stock_name
+GROUP BY stock_name;
