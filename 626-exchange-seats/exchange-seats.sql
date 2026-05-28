c7fee332bfef5f1374a12fa34571e08ff413e4fa
Upal Pahari <upalthisside@gmail.com>
2026-05-28 13:51:54 +0530

    Time: 340 ms (74.49%) | Memory: 0B (100.00%) - LeetSync

--- 626-exchange-seats/exchange-seats.sql
+++ 626-exchange-seats/exchange-seats.sql
@@ -1,14 +1,13 @@
 SELECT
     IF(
-        MOD(id, 2) = 1
-        AND id = (SELECT COUNT(*) FROM Seat),
-
+        id = (SELECT MAX(id) FROM seat) AND MOD(id, 2) = 1,
+        
         id,
-
+        
         IF(MOD(id, 2) = 1, id + 1, id - 1)
     ) AS id,
-
+    
     student
 
-FROM Seat
+FROM seat
 ORDER BY id;
