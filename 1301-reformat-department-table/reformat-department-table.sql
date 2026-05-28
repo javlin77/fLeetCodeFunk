43eb3f798c72f38bca8a2bf70dc24ba1e93af7dc
Upal Pahari <upalthisside@gmail.com>
2026-05-28 14:53:11 +0530

    Time: 496 ms (83.46%) | Memory: 0B (100.00%) - LeetSync

--- 1301-reformat-department-table/reformat-department-table.sql
+++ 1301-reformat-department-table/reformat-department-table.sql
@@ -1,17 +1,17 @@
 # Write your MySQL query statement below
 SELECT id, 
-    SUM(IF(month="Jan", revenue, NULL)) AS Jan_Revenue, 
-    SUM(IF(month="Feb", revenue, NULL)) AS Feb_Revenue, 
-    SUM(IF(month="Mar", revenue, NULL)) AS Mar_Revenue,
-    SUM(IF(month="Apr", revenue, NULL)) AS Apr_Revenue,
-    SUM(IF(month="May", revenue, NULL)) AS May_Revenue,
-    SUM(IF(month="Jun", revenue, NULL)) AS Jun_Revenue,
-    SUM(IF(month="Jul", revenue, NULL)) AS Jul_Revenue,
-    SUM(IF(month="Aug", revenue, NULL)) AS Aug_Revenue,
-    SUM(IF(month="Sep", revenue, NULL)) AS Sep_Revenue,
-    SUM(IF(month="Oct", revenue, NULL)) AS Oct_Revenue,
-    SUM(IF(month="Nov", revenue, NULL)) AS Nov_Revenue,
-    SUM(IF(month="Dec", revenue, NULL)) AS Dec_Revenue
+    MAX(IF(month="Jan", revenue, NULL)) AS Jan_Revenue, 
+    MAX(IF(month="Feb", revenue, NULL)) AS Feb_Revenue, 
+    MAX(IF(month="Mar", revenue, NULL)) AS Mar_Revenue,
+    MAX(IF(month="Apr", revenue, NULL)) AS Apr_Revenue,
+    MAX(IF(month="May", revenue, NULL)) AS May_Revenue,
+    MAX(IF(month="Jun", revenue, NULL)) AS Jun_Revenue,
+    MAX(IF(month="Jul", revenue, NULL)) AS Jul_Revenue,
+    MAX(IF(month="Aug", revenue, NULL)) AS Aug_Revenue,
+    MAX(IF(month="Sep", revenue, NULL)) AS Sep_Revenue,
+    MAX(IF(month="Oct", revenue, NULL)) AS Oct_Revenue,
+    MAX(IF(month="Nov", revenue, NULL)) AS Nov_Revenue,
+    MAX(IF(month="Dec", revenue, NULL)) AS Dec_Revenue
     
 FROM Department
 GROUP BY id
