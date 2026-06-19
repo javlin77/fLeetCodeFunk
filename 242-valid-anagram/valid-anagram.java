569c180d0fbee31373c0ed489725104614d9c8cb
Upal Pahari <upalthisside@gmail.com>
2026-06-19 21:22:09 +0530

    Time: 15 ms (24.26%) | Memory: 46.3 MB (45.78%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,14 +1,17 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if(s.length() != t.length()) return false;
-
-        int[] count = new int[26];
-        for(int i = 0; i < s.length(); i++){
-            count[s.charAt(i) - 'a']++;
-            count[t.charAt(i) - 'a']--;
+        if(s.length() != t.length()){
+            return false;
         }
-        for(int c : count){
-            if(c != 0) return false;
+        Map<Character, Integer> m=new HashMap<>();
+        for(char c: s.toCharArray()){
+            m.put(c,m.getOrDefault(c,0)+1);
+        }
+        for(char c: t.toCharArray()){
+            m.put(c,m.getOrDefault(c,0)-1);
+            if(m.get(c)<0){
+                return false;
+            }
         }
         return true;
     }
