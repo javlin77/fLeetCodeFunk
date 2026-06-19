5ae4125d8ff0d526f99ec90c6ecba10121e2e647
Upal Pahari <upalthisside@gmail.com>
2026-06-19 21:27:17 +0530

    Time: 14 ms (26.86%) | Memory: 46.7 MB (15.64%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -5,11 +5,11 @@ class Solution {
         }
         Map<Character, Integer> m=new HashMap<>();
         for(char c: s.toCharArray()){
-            m.put(c,m.getOrDefault(c,0)+1);
+            m.put(c,m.getOrDefault(c,0)-1);
         }
         for(char c: t.toCharArray()){
-            m.put(c,m.getOrDefault(c,0)-1);
-            if(m.get(c)<0){
+            m.put(c,m.getOrDefault(c,0)+1);
+            if(m.get(c)>0){
                 return false;
             }
         }
