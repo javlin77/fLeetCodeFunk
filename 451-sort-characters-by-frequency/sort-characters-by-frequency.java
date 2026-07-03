1773864597d8954c4ed35b6f7f598a22eebec294
Upal Pahari <upalthisside@gmail.com>
2026-07-03 18:02:01 +0530

    Time: 15 ms (45.72%) | Memory: 46.5 MB (77.66%) - LeetSync

--- 451-sort-characters-by-frequency/sort-characters-by-frequency.java
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.java
@@ -4,20 +4,15 @@ class Solution {
         for (char c : s.toCharArray()) {
             m.put(c, m.getOrDefault(c, 0) + 1);
         }
-        List<Character> l = new ArrayList<>();
-        for (char c : s.toCharArray()) {
-            l.add(c);
-        }
+        List<Character> l = new ArrayList<>(m.keySet());
         l.sort((a, b) -> {
-            if (!m.get(a).equals(m.get(b))) {
-                return m.get(b) - m.get(a); 
-            } else {
-                return a - b; 
-            }
+            return m.get(b) - m.get(a);
         });
         StringBuilder sb = new StringBuilder();
         for (char c : l) {
-            sb.append(c);
+            for (int i = 0; i < m.get(c); i++) {
+                sb.append(c);
+            }
         }
         return sb.toString();
     }
